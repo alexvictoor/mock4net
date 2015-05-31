dec0031c729f6e5dcec46017294359a5240055f7
alexvictoor <alexvictoor@gmail.com>
2015-05-31 22:12:02 +0200

    Mock server glue

---
+++ Mock4Net.Core.Tests/FluentMockServerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace Mock4Net.Core.Tests
+{
+    [TestFixture]
+    [Timeout(5000)]
+    public class FluentMockServerTest
+    {
+        private FluentMockServer _server;
+
+        [Test]
+        public async void Should_respond_to_request()
+        {
+            // given
+            _server = FluentMockServer.Start();
+            _server
+                .ForRequest("/foo")
+                    .Get()
+                .Respond()
+                    .WithStatusCode(200)
+                    .WithBody(@"{ msg: ""Hello world!""}");
+            // when
+            var response 
+                = await new HttpClient().GetStringAsync("http://localhost:" + _server.Port + "/foo");
+            // then
+            Check.That(response).IsEqualTo(@"{ msg: ""Hello world!""}");
+        }
+
+        [Test]
+        public async void Should_respond_404_for_unexpected_request()
+        {
+            // given
+            _server = FluentMockServer.Start();
+            // when
+            var response
+                = await new HttpClient().GetAsync("http://localhost:" + _server.Port + "/foo");
+            // then
+            Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.NotFound);
+            Check.That((int)response.StatusCode).IsEqualTo(404);
+        }
+
+        [TearDown]
+        public void ShutdownServer()
+        {
+            _server.Stop();
+        }
+    }
+}
--- Mock4Net.Core/FluentMockServer.cs
+++ Mock4Net.Core/FluentMockServer.cs
@@ -1,28 +1,78 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Mock4Net.Core.Http;
 
 namespace Mock4Net.Core
 {
     public class FluentMockServer
     {
 
+        private readonly TinyHttpServer _httpServer;
+        private readonly IList<Route> _routes = new List<Route>(); 
+        private readonly HttpListenerRequestMapper _requestMapper = new HttpListenerRequestMapper();
+        private readonly HttpListenerResponseMapper _responseMapper = new HttpListenerResponseMapper();
+        private readonly int _port;
+
         private FluentMockServer(int port)
         {
-            
+            _httpServer = new TinyHttpServer("http://localhost:" + port + "/", HandleRequest);
+            _port = port;
+            _httpServer.Start();
         }
 
-        public FluentMockServer StartHttpServer(int port = 0)
+        public int Port
         {
-            return null;
+            get { return _port; }
         }
-        
+
+        private void RegisterRoute(Route route)
+        {
+            lock (((ICollection)_routes).SyncRoot)
+            {
+                _routes.Add(route);
+            }
+        }
+
+        private void HandleRequest(HttpListenerContext ctx)
+        {
+            var request = _requestMapper.Map(ctx.Request);
+
+            var targetRoute = _routes.FirstOrDefault(route => route.IsRequestHandled(request));
+            if (targetRoute == null)
+            {
+                ctx.Response.StatusCode = 404;
+                var content = Encoding.UTF8.GetBytes("<html><body>Mock Server: page not found</body></html>");
+                ctx.Response.OutputStream.Write(content, 0, content.Length);
+            }
+            else
+            {
+                _responseMapper.Map(targetRoute.Response, ctx.Response);    
+            }
+            ctx.Response.Close();
+        }
+
+        public static FluentMockServer Start(int port = 0)
+        {
+            if (port == 0)
+            {
+                port = Ports.FindFreeTcpPort();
+            }
+            return new FluentMockServer(port);
+        }
+
+        public void Stop()
+        {
+            _httpServer.Stop();
+        }
         
         public IVerbRequestBuilder ForRequest(string url = "*")
         {
-            return null;
+            return new RouteBuilder(RegisterRoute, url);
         }
 
         public void Test()
