03d266b01f232186a5382c0b61dcc9bc31b2e1e8
alexvictoor <alexvictoor@gmail.com>
2015-06-06 22:35:40 +0200

    Generate assembly version during build

--- BuildSupport/Properties/AssemblyInfo.cs
+++ BuildSupport/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mock4Net.Core")]
-[assembly: AssemblyDescription("A lightweight http server for mocking web services and REST resources")]
+[assembly: AssemblyTitle("BuildSupport")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alexandre Victoor")]
-[assembly: AssemblyProduct("Mock4Net.Core")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuildSupport")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("53ad53a7-869f-4c2d-a1ff-7aa36cbad051")]
+[assembly: Guid("7471aa76-d8a5-48b3-9995-539d3392dd0e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,8 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-/*[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0-alpha")]
-*/
-
