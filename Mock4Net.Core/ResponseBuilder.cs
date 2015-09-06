4445ef888f748bd230920e98843e74eabaae8bb0
alexvictoor <alexvictoor@gmail.com>
2015-09-06 15:30:15 +0200

    code clean-up

--- Mock4Net.Core/ResponseBuilder.cs
+++ Mock4Net.Core/ResponseBuilder.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mock4Net.Core
 {
-    /*public interface IStatusCodeResponseBuilder : IHeadersResponseBuilder
-    {
-        IHeadersResponseBuilder WithStatusCode(int code);
-    }*/
-
+   
     public interface IHeadersResponseBuilder : IBodyResponseBuilder
     {
         IHeadersResponseBuilder WithHeader(string name, string value);
